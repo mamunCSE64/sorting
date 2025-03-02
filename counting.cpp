28dc2bb9a52a01121580a96ca48612d4ac60631c
Abdul Al Mamun <124201619+mamunCSE64@users.noreply.github.com>
2025-03-02 20:13:27 +0600

    Create counting.cpp

---
+++ counting.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define gcd __gcd
+#define int_string to_string
+#define string_int stoi
+#define mn(v) *min_element(v.begin(), v.end())
+#define mx(v) *max_element(v.begin(), v.end())
+#define index_character s.find('character')
+#define countxchar count(s.begin(), s.end(), 'x')
+#define index_ofX_vector find(v.begin(), v.end(), x) - v.begin()
+#define point cout << fixed << setprecision(10)
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define n1 cout << "-1" << endl
+#define ok cout << "ok" nl
+#define nl << endl
+#define sp << " "
+#define fi first
+#define se second
+#define inf LLONG_MAX
+#define mod %1000000007
+// ordered_set
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds;   
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
+// bitsize-> ll size=log2(n)+1 (last set bit=size-1;/ /Last bit->__lg(value); )
+// first bit-> name._Find_First()
+// next bit after x-> name._Find_next()
+// freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+// BesidesDuplicateCharacterEraseInString s.erase(unique(s.begin(), s.end()), s.end());
+// Upper/lower-> transform(s.begin(), s.end(), s.begin(), ::toupper/tolower);
+
+ll i, j, k, n, m, e=1e9;
+const ll N = 2e6+5;
+double pi=acos(-1);
+bool cmp(pair<ll,ll> a,pair<ll,ll> b){
+    if(a.fi==b.fi) return (a.se>b.se); return (a.fi>b.fi);}
+
+//
+// Don't get stuck on a single approach for long, think of multiple ways
+// You will destroy your dream if you stay in your comfort zone
+// **********************|| Main Code ||********************************
+//
+ 
+//  g++ test.cpp -o a && ./a
+//  g++ -o a test.cpp && ./a
+//  g++ -o a test.cpp && a.e
+ 
+
+int main()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    // point;
+    ll test = 1; 
+    // cin >> test;    
+    again: 
+    while (test--)
+    {   
+        ll n;
+        cin>>n;
+        vector<ll> v(n);
+        ll x=0;
+        for(i=0;i<n;i++){
+            cin>>v[i];
+            x=max(x,v[i]);
+        }
+        vector<ll> count(x+1);
+        for(i=0;i<n;i++){
+            count[v[i]]++;
+        }
+        vector<ll> psum(x+1);
+        ll sum=0;
+        for(i=0;i<=x;i++){
+            sum+=count[i];
+            psum[i]=sum;
+        }
+        vector<ll> ans(n);
+        for(i=0;i<n;i++){
+            psum[v[i]]--;
+            ans[psum[v[i]]]=v[i];
+        }
+        for(auto x: ans){
+            cout << x sp;
+        }
+        cout nl;
+    }     
+}
